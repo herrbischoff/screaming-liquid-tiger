bade0692d2df439dbd876825b3ecccf6236e4458
Marcel Bischoff <marcel@herrbischoff.com>
2015-12-15 17:35:27 +0100

    Add tag reading and image functionality

--- index.php
+++ index.php
@@ -12,22 +12,35 @@
  * @since      File available since Release 0.1.0
  *****************************************************************************/
 
+/**
+ * Make sure that unicode characters in file names are not dropped.
+ */
+setlocale(LC_CTYPE, "C.UTF-8");
+
 /******************************************************************************
  * Configuration Start
  *****************************************************************************/
 
 /**
+ * Whether to check for ffmpeg
+ */
+$ffmpeg_check = true;
+
+/**
  * Feed info
  *
- * Basic feed information, all of which are mandatory.
+ * Basic feed information.
+ *
  * description: basically anything you want, may appear in feed reader
  * link: dummy or real URL
  * title: your feed title as it appears in the feed reader
+ * image: main image for feed (optional)
  */
 $conf = array(
     'description' => 'Personal Audiobook Feed',
     'link'        => 'http://www.example.com',
-    'title'       => 'Audiobook Podcast'
+    'title'       => 'Audiobook Podcast',
+    'image'       => 'cast.jpg'
 );
 
 /**
@@ -63,6 +76,29 @@ if (!defined('PHP_VERSION_ID') || PHP_VERSION_ID < 50400) :
 endif;
 
 /**
+ * Create temp folder if not present
+ */
+if (!file_exists('./tmp')) :
+    if (!mkdir('./tmp', 0777)) :
+        die('Temp folder creation failed!');
+    endif;
+endif;
+
+/**
+ * Get ffmpeg path
+ */
+$ffmpeg = '';
+if ($ffmpeg_check) :
+    $ffmpeg_global = trim(shell_exec('which ffmpeg'));
+    $ffmpeg_static = file_exists('./ffmpeg');
+    if ($ffmpeg_global) $ffmpeg = $ffmpeg_global;
+    if ($ffmpeg_static) $ffmpeg = './ffmpeg';
+    if (!$ffmpeg) :
+        die("For automatic tag reading functionality, you need ffmpeg. Either install it globally on your server or download the correct static binary for your system here:\n\nhttps://ffmpeg.org/download.html\n\nPut it in the same folder as this script. If you download a static build, make sure to configure your web server to block access to the binary for visitors.\n\nIn case you don't want this functionality, set the \$ffmpeg_check variable to 'false'.");
+    endif;
+endif;
+
+/**
  * Format date according to RFC 822
  */
 $date_fmt = 'D, d M Y H:i:s e';
@@ -77,8 +113,15 @@ $prot = ($_SERVER['HTTPS'] != "on") ? 'http://' : 'https://';
  */
 $base_url = str_replace("index.php", "", $prot . $_SERVER["HTTP_HOST"] . $_SERVER["PHP_SELF"]);
 
+/**
+ * Set feed image if present
+ * */
+if ($conf['image'] && file_exists($conf['image'])) :
+    $castimg_url = $base_url . rawurlencode($conf['image']);
+endif;
+
 printf("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
-printf("<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\">\n");
+printf("<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\" xmlns:itunes=\"http://www.itunes.com/dtds/podcast-1.0.dtd\">\n");
 printf("\t<channel>\n");
 printf("\t\t<title>%s</title>\n", $conf['title']);
 printf("\t\t<link>%s</link>\n", $conf['link']);
@@ -86,6 +129,9 @@ printf("\t\t<description>%s</description>\n", $conf['description']);
 printf("\t\t<pubDate>%s</pubDate>\n", date($date_fmt));
 printf("\t\t<lastBuildDate>%s</lastBuildDate>\n", date($date_fmt));
 printf("\t\t<atom:link href=\"%s\" rel=\"self\" type=\"application/rss+xml\"/>\n", $base_url);
+if ($castimg_url) :
+    printf("\t\t<itunes:image href=\"%s\"/>\n", $castimg_url);
+endif;
 
 /**
  * Open file handler for current directory
@@ -101,16 +147,41 @@ if ($handle = opendir('.')) :
          * Make sure file matches extensions from array
          */
         if (array_key_exists(pathinfo($entry, PATHINFO_EXTENSION), $exts)) :
-            printf("\t\t<item>\n");
             $p = pathinfo($entry);
-            printf("\t\t\t<title>%s</title>\n", $p['filename']);
-            printf("\t\t\t<guid>%s</guid>\n", $base_url . rawurlencode($entry));
+            $filename = $p['filename'];
+            $fileimg_path = escapeshellarg('./tmp/' . $filename . '.jpg');
+            $fileimg_url = $base_url . 'tmp/' . rawurlencode($filename . '.jpg');
+
+            ob_start();
+            $cmd = $ffmpeg . ' -y -i ' . escapeshellarg($entry) . ' ' . $fileimg_path . ' 2>&1';
+            passthru($cmd);
+            $ffmpeg_out = ob_get_contents();
+            ob_end_clean();
+
+            if ($ffmpeg) :
+                preg_match('/artist.*: (.*)/', $ffmpeg_out, $artist_matches);
+                preg_match('/album.*: (.*)/', $ffmpeg_out, $album_matches);
+                preg_match('/Duration: (.*?),/', $ffmpeg_out, $duration_matches);
+                $title = $artist_matches[1] . ' - ' . $album_matches[1];
+                $duration = $duration_matches[1];
+            else :
+                $title = $p['filename'];
+            endif;
+            printf("\t\t<item>\n");
+            printf("\t\t\t<title>%s</title>\n", $title);
+            printf("\t\t\t<guid isPermalink=\"false\">%s</guid>\n", $base_url . rawurlencode($entry));
             printf("\t\t\t<enclosure url=\"%s\" length=\"%s\" type=\"%s\"/>\n",
                 $base_url . rawurlencode($entry),
                 filesize($entry),
                 $exts[$p['extension']]
             );
             printf("\t\t\t<pubDate>%s</pubDate>\n", date($date_fmt, filemtime($entry)));
+            if ($ffmpeg) :
+                if (file_exists('./tmp/' . $filename . '.jpg')) :
+                    printf("\t\t\t<itunes:image href=\"%s\"/>\n", $fileimg_url);
+                endif;
+                printf("\t\t\t<itunes:duration>%s</itunes:duration>\n", $duration);
+            endif;
             printf("\t\t</item>\n");
         endif;
 
