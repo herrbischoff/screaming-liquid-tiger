c3229c26608f8ac309e302b7ad1c039e895fafa0
Marcel Bischoff <marcel@herrbischoff.com>
2016-02-25 14:54:12 +0100

    Code refactoring to use SimpleXMLElement
    Initially the script was using printf, now it uses a correct procedure
    to generate the feed via SimpleXMLElement. This produces more readable
    code and excludes the possibility for typos messing up the XML.

--- index.php
+++ index.php
@@ -120,19 +120,26 @@ if ($conf['image'] && file_exists($conf['image'])) :
 endif;
 
 /**
- * Output feed header
+ * Construct feed
  */
-printf("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
-printf("<rss version=\"2.0\" xmlns:atom=\"http://www.w3.org/2005/Atom\" xmlns:itunes=\"http://www.itunes.com/dtds/podcast-1.0.dtd\">\n");
-printf("\t<channel>\n");
-printf("\t\t<title>%s</title>\n", $conf['title']);
-printf("\t\t<link>%s</link>\n", $conf['link']);
-printf("\t\t<description>%s</description>\n", $conf['description']);
-printf("\t\t<pubDate>%s</pubDate>\n", date($date_fmt));
-printf("\t\t<lastBuildDate>%s</lastBuildDate>\n", date($date_fmt));
-printf("\t\t<atom:link href=\"%s\" rel=\"self\" type=\"application/rss+xml\"/>\n", $base_url);
-if ($castimg_url) :
-    printf("\t\t<itunes:image href=\"%s\"/>\n", $castimg_url);
+
+$xmlstr = '<?xml version="1.0" encoding="UTF-8"?><rss/>';
+$rss = new SimpleXMLElement($xmlstr);
+$rss->addAttribute('version', '2.0');
+$rss->addAttribute('xmlns:xmlns:atom', 'http://www.w3.org/2005/Atom');
+$rss->addAttribute('xmlns:xmlns:itunes', 'http://www.itunes.com/dtds/podcast-1.0.dtd');
+$channel = $rss->addChild('channel');
+$channel->addChild('title', $conf['title']);
+$channel->addChild('link', $conf['link']);
+$channel->addChild('description', $conf['description']);
+$channel->addChild('pubDate', date($date_fmt));
+$channel->addChild('lastBuildDate', date($date_fmt));
+$atomlink = $channel->addChild('atom:link');
+$atomlink->addAttribute('rel', 'self');
+$atomlink->addAttribute('type', 'application/rss+xml');
+if (isset($castimg_url)) :
+    $itunes_image = $channel->addChild('xmlns:itunes:image');
+    $itunes_image->addAttribute('href', $castimg_url);
 endif;
 
 /**
@@ -176,22 +183,20 @@ if ($handle = opendir('.')) :
             endif;
 
             /**
-             * Output feed item
+             * Contruct feed item
              */
-            printf("\t\t<item>\n");
-            printf("\t\t\t<title>%s</title>\n", $title);
-            printf("\t\t\t<guid isPermalink=\"false\">%s</guid>\n",
-                $base_url . rawurlencode($entry));
-            printf("\t\t\t<enclosure url=\"%s\" length=\"%s\" type=\"%s\"/>\n",
-                $base_url . rawurlencode($entry),
-                filesize($entry),
-                $exts[$p['extension']]
-            );
-            printf("\t\t\t<pubDate>%s</pubDate>\n", date($date_fmt, filemtime($entry)));
+            $item = $channel->addChild('item');
+            $item->addChild('title', $title);
+            $guid = $item->addChild('guid', $base_url . rawurlencode($entry));
+            $guid->addAttribute('isPermalink', 'false');
+            $enclosure = $item->addChild('enclosure');
+            $enclosure->addAttribute('url', $base_url . rawurlencode($entry));
+            $enclosure->addAttribute('length', filesize($entry));
+            $enclosure->addAttribute('type', $exts[$p['extension']]);
+            $item->addChild('pubDate', date($date_fmt, filemtime($entry)));
             if ($mediainfo) :
-                printf("\t\t\t<itunes:duration>%s</itunes:duration>\n", $duration);
+                $item->addChild('xmlns:itunes:duration', $duration);
             endif;
-            printf("\t\t</item>\n");
 
         endif;
 
@@ -207,5 +212,7 @@ endif;
  */
 closedir($handle);
 
-printf("\t</channel>\n");
-printf("</rss>");
+/**
+ * Output feed
+ */
+echo $rss->asXML();
